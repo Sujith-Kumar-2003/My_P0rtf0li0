c698c0ba55c733cc23e67d6c9721038333a63dc8
deBauch_Casanova <sujithkumar2003k@gmail.com>
2024-07-03 18:17:37 -0400

    good set of changes made to the site.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/My_P0rtf0li0/', // Replace with your repository name
 })
+
